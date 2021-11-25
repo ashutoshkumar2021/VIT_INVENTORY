d1cfcaf810ab7ef60db9955108143a31c81b54de
ashutoshkumar2021 <80633589+ashutoshkumar2021@users.noreply.github.com>
2021-11-26 00:44:51 +0530

    Create mapvit.js

---
+++ mapvit.js
@@ -0,0 +1,55 @@
+document.getElementById("ashu0").style.display="none";
+document.getElementById("ashu1").style.display="none";
+document.getElementById("ashu2").style.display="none";
+document.getElementById("ashu3").style.display="none";
+document.getElementById("ashu4").style.display="none";
+document.getElementById("ashu5").style.display="none";
+document.getElementById("ashu6").style.display="none";
+document.getElementById("ashu7").style.display="none";
+document.getElementById("ashu8").style.display="none";
+document.getElementById("ashu9").style.display="none";
+
+
+
+document.querySelectorAll("button")[3].addEventListener("click",handelclick1);
+
+function handelclick1(){
+    document.getElementById("ashu0").style.display="block";
+    
+}    
+document.querySelectorAll("button")[4].addEventListener("click",handelclick2);
+function handelclick2(){
+    document.getElementById("ashu1").style.display="block";
+}  
+document.querySelectorAll("button")[5].addEventListener("click",handelclick3);
+function handelclick3(){
+    document.getElementById("ashu3").style.display="block";
+}  
+document.querySelectorAll("button")[6].addEventListener("click",handelclick4);
+function handelclick4(){
+    document.getElementById("ashu2").style.display="block";
+}  
+document.querySelectorAll("button")[7].addEventListener("click",handelclick5);
+function handelclick5(){
+    document.getElementById("ashu4").style.display="block";
+}  
+document.querySelectorAll("button")[8].addEventListener("click",handelclick6);
+function handelclick6(){
+    document.getElementById("ashu5").style.display="block";
+}  
+document.querySelectorAll("button")[9].addEventListener("click",handelclick7);
+function handelclick7(){
+    document.getElementById("ashu6").style.display="block";
+}  
+document.querySelectorAll("button")[10].addEventListener("click",handelclick8);
+function handelclick8(){
+    document.getElementById("ashu7").style.display="block";
+}  
+document.querySelectorAll("button")[11].addEventListener("click",handelclick9);
+function handelclick9(){
+    document.getElementById("ashu8").style.display="block";
+}  
+document.querySelectorAll("button")[12].addEventListener("click",handelclick10);
+function handelclick10(){
+    document.getElementById("ashu9").style.display="block";
+}  
